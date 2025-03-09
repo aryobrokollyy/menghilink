60b43025f9d3ee3593681125de886fc44274a610
Aryo Brokolly <aryobrokolly92@gmail.com>
2025-03-09 22:30:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,10 +7,11 @@ MODEL=/usr/lib/lua/luci/model/cbi
 CONT=/usr/lib/lua/luci/controller
 URL=https://raw.githubusercontent.com/aryobrokollyy/menghilink/main
 URL1=https://raw.githubusercontent.com/saputribosen/hilink/main
+MAX_RETRIES=3
 
-finish(){
-clear
-	echo ""
+finish() {
+    clear
+    echo ""
     echo "INSTALL SUCCESSFULLY ;)"
     echo ""
     echo "Untuk Menjalankan Ketik m dan enter di terminal"
@@ -21,19 +22,40 @@ clear
     echo ""
 }
 
-download_files()
-{
-    	clear
-  	echo "Downloading files from repo hilink mak cling..."
-   	wget -O $DIR/m $URL/usr/bin/m && chmod +x $DIR/m
- 	wget -O $DIR/hilink $URL/usr/bin/hilink && chmod +x $DIR/hilink
-  	wget -O $DIR/balong-nvtool $URL/usr/bin/balong-nvtool && chmod +x $DIR/balong-nvtool
-   	wget -O $DIR/updatehilink $URL/usr/bin/balong-nvtool && chmod +x $DIR/updatehilink
-        wget -O $CONF/hilink $URL1/hilink
-	wget -O $DIR/elink $URL1/elink.sh && chmod +x $DIR/elink
-        wget -O $CONT/hilink.lua $URL1/controller/hilink.lua && chmod +x $CONT/hilink.lua
-        wget -O $MODEL/hilink.lua $URL1/cbi/hilink.lua && chmod +x $MODEL/hilink.lua
-    	finish
+download_file() {
+    local dest=$1
+    local src=$2
+    local retries=0
+
+    while [ $retries -lt $MAX_RETRIES ]; do
+        wget -O "$dest" "$src"
+        if [ $? -eq 0 ] && [ -s "$dest" ]; then
+            chmod +x "$dest"
+            return 0
+        fi
+        echo "Gagal mengunduh $src, mencoba ulang... ($((retries + 1))/$MAX_RETRIES)"
+        retries=$((retries + 1))
+        sleep 2
+    done
+
+    echo "Gagal mengunduh $src setelah $MAX_RETRIES percobaan. Menghentikan instalasi."
+    exit 1
+}
+
+download_files() {
+    clear
+    echo "Downloading files from repo hilink mak cling..."
+
+    download_file "$DIR/m" "$URL/usr/bin/m"
+    download_file "$DIR/hilink" "$URL/usr/bin/hilink"
+    download_file "$DIR/balong-nvtool" "$URL/usr/bin/balong-nvtool"
+    download_file "$DIR/updatehilink" "$URL/usr/bin/balong-nvtool"
+    download_file "$CONF/hilink" "$URL1/hilink"
+    download_file "$DIR/elink" "$URL1/elink.sh"
+    download_file "$CONT/hilink.lua" "$URL1/controller/hilink.lua"
+    download_file "$MODEL/hilink.lua" "$URL1/cbi/hilink.lua"
+
+    finish
 }
 
 echo ""
